91eef3f7b9fb1f5008973a3378dd024d1ef0e189
gsikotidis <gsikotidis@iti.gr>
2017-07-27 11:12:19 +0300

    remove json.hpp from cl_sim.cpp and gs.cpp

--- src/cl_sim.cpp
+++ src/cl_sim.cpp
@@ -11,7 +11,6 @@
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
-#include <json.hpp>
 #include <string>
 #include <vector>
 
--- src/gs.cpp
+++ src/gs.cpp
@@ -7,7 +7,6 @@
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
-#include <json.hpp>
 #include <sstream>
 #include <vector>
 
