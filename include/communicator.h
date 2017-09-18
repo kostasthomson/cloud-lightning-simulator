a62cc5c1c0816b8cb6c06f28f958a5926eb4e7e3
gsikotidis <gsikotidis@iti.gr>
2017-09-18 15:42:03 +0300

    cosmetic changes to communicator.h

--- include/communicator.h
+++ include/communicator.h
@@ -13,14 +13,14 @@ class communicator
 {
  public:
   /// Sends configuration parameters from the gateway to the cells
-  void simulationParameters(class siminputs& si, const int& rank, const int& numberOfTasks, const MPI_Comm& Comm);
+  void simulationParameters(class siminputs& si, const int& rank, const int& clusterSize, const MPI_Comm& Comm);
 
   /// Retrieves statistics from the cells
-  void cellStatistics(const gs* gates, const cell* clCell, const int& rank, const int& numberOfTasks,
+  void cellStatistics(const gs* gates, const cell* clCell, const int& rank, const int& clusterSize,
                       const MPI_Comm& Comm);
 
   /// Sends tasks to the appropriate cells for further processing
-  void taskParameters(list<task>& jobs, const int& rank, const int& numberOfTasks, const int* commCell,
+  void taskParameters(list<task>& jobs, const int& rank, const int& clusterSize, const int* commCell,
                       const MPI_Comm& Comm);
 };
 #endif
