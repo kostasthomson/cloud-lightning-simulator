5c4a60b7f8297f058bbe4fb7eaee31e69ad9384c
gsikotidis <gsikotidis@iti.gr>
2017-11-28 15:33:59 +0200

    fix dates on copyrights

--- src/vRM.cpp
+++ src/vRM.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2015 The CloudLightning Simulation Framework Authors. All
+/* Copyright 2017 The CloudLightning Simulation Framework Authors. All
 Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
